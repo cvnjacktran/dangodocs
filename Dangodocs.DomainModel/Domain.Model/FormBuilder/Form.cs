ed9ce5dec894385291959d30e7766f8ffadf27fb
cvnjacktran <jack.tran.tmt@gmail.com>
2016-06-26 23:18:59 +0700

    a

---
+++ Dangodocs.DomainModel/Domain.Model/FormBuilder/Form.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dangodocs.DomainModel.Domain.Model.FormBuilder
+{
+    public class Form
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
